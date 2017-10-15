efc1ef925fa93b1b5ea72ee53f88b48b44630659
Vishal Ranjan <vishivish18@gmail.com>
2017-10-15 22:14:12 +0530

    more changes

--- src/components/credits.jsx
+++ src/components/credits.jsx
@@ -8,7 +8,7 @@ class Credits extends React.Component {
     render() {
         return (
       		<div style={{position:'fixed',bottom:5, left: 2, fontSize:12, fontWeight:300}}>
-          <div style={{ fontSize: '12px',color: '#aaa',padding: '10px 20px',letterSpacing: '0.5px'}}>Developed by <a style={{color:'#2998ff',textDecoration:'none',fontWeight:400}} target="_blank" href="http://www.roshanraj.com">Roshan</a> & <a style={{color:'#2998ff',textDecoration:'none',fontWeight:400}}  target="_blank" href="https://www.vishalranjan.in">Vishal</a> | Designed by <a style={{color:'#2998ff',textDecoration:'none',fontWeight:400}} target="_blank" href="http://www.sankalpsinha.com">Sankalp</a> </div>
+          <div style={{ fontSize: '12px',color: '#aaa',padding: '10px 20px',letterSpacing: '0.5px'}}>Designed by <a style={{color:'#2998ff',textDecoration:'none',fontWeight:400}} target="_blank" href="http://www.sankalpsinha.com">Sankalp</a> | Developed by <a style={{color:'#2998ff',textDecoration:'none',fontWeight:400}} target="_blank" href="http://www.roshanraj.com">Roshan</a> & <a style={{color:'#2998ff',textDecoration:'none',fontWeight:400}}  target="_blank" href="https://www.vishalranjan.in">Vishal</a> </div>
 		     </div>
 
     	)
